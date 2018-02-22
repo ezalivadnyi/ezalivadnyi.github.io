3dd3ebed60dd3bdb0682069dfa2ddfc7a61ca82a
Eugene Zalivadnyi <zalik132@gmail.com>
2018-02-22 17:44:25 +0200

    fix

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
 $(document).ready(function () {
-
+    /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+    particlesJS.load('particles-js', 'particles.json', function() {
+        console.log('callback - particles.js config loaded');
+    });
 });
+
