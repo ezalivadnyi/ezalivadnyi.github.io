58c44b4f14df48ed3a263554f0a834a0ad427add
Eugene Zalivadnyi <eugene.zalivadnyi@instandart.com>
2020-04-29 04:12:26 +0300

    refactoring and clean

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,8 @@
 $(document).ready(function () {
 
-    /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
     particlesJS.load('particles-js', 'js/particles.json', function() {
         console.log('callback - particles.js config loaded');
     });
 
-    var myFullpage = new fullpage('#fullpage', {
-		sectionsColor: [
-		    'rgba(207, 226, 255, 0.75)',
-            'rgba(0, 0, 0, 0.23)',
-            'rgba(73, 119, 204, 0.42)',
-            'rgba(38, 14, 236, 0.18)',
-            'rgba(16, 255, 0, 0.22)',
-        ],
-        anchors:['page-header', 'skillsPage', 'contactsPage', 'page-footer'],
-        menu: '#menu',
-        lazyLoad: true
-	});
-
-    // Scroll Down The Window when click on first screen's button.
-    // var $scroll_btn = document.getElementById('js-scroll-down');
-    // var $js_skills_screen = document.getElementById('js-skills-screen');
-    // $scroll_btn.addEventListener("click", function (ev) {
-    //     var position = $js_skills_screen.getBoundingClientRect().top + window.scrollY;
-    //     console.log(position);
-    //
-    //     window.scrollTo(0, position);
-    // })
 });
 
