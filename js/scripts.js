746d55e0b1aa8cf930113ffa8c5014d6db9a605b
Eugene Zalivadnyi <eugene.zalivadnyi@gmail.com>
2020-12-26 00:08:32 +0200

    fix

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
 
+    $('#jsCurrentYear').text(new Date().getFullYear());
+ 
     particlesJS.load('particles-js', 'js/particles.json', function() {
         console.log('callback - particles.js config loaded');
     });
